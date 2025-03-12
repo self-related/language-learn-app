9bfeab39cbc63c4117f35d3b8079ed3dc1a96b3c
Nekit <nick.misiura@yandex.ru>
2025-03-12 12:13:25 +0300

    init vite + tailwindcss

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='w-full h-[55px] bg-[#252525] text-white flex justify-center items-center'>
+        Test
+    </div>
+  )
+}
+
+export default App
