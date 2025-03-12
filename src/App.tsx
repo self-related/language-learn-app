c0f22eeb7302cc2d54b1949d003e657879999b12
Nekit <nick.misiura@yandex.ru>
2025-03-12 12:55:32 +0300

    styling + some initial elements

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,29 @@ import './App.css'
 function App() {
 
   return (
-    <div className='w-full h-[55px] bg-[#252525] text-white flex justify-center items-center'>
-        Test
-    </div>
+    <>
+        <nav className='w-full min-h-[55px] bg-[#151515] flex justify-center items-center'>
+            Language Learning App
+        </nav>
+
+        <main>
+            <div>
+                <textarea placeholder='type a word or a phrase' 
+                    className='bg-[#505050] mt-4 mb-2 ml-2 px-2 py-1 w-[225px] resize-none'
+                />
+                <button></button>
+            </div>
+
+            <div>
+                <div
+                    className='bg-[#505050] my-2 ml-2 px-2 py-1 min-h-[70px] h-min w-[225px]'
+                >
+                </div>
+                <button></button>
+            </div>
+        </main>
+    </>
+    
   )
 }
 
