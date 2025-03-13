7e66a085512de372518fb11e8a598918b118f8c2
Nekit <nick.misiura@yandex.ru>
2025-03-13 13:54:43 +0300

    add outline for 'Add' button when focused

--- src/components/LearningSection.tsx
+++ src/components/LearningSection.tsx
@@ -22,7 +22,7 @@ export default function LearningSection() {
                     className='bg-[#505050] hover:bg-[#606060] accent-orange-400 my-2 px-2 py-1 min-h-[70px] h-min w-[225px] rounded-sm resize-none'
                 />
 
-                <button className='px-2 block ml-auto bg-gray-700 hover:bg-gray-600 active:bg-gray-800 accent-orange-400 cursor-pointer rounded-sm'
+                <button className='px-2 block ml-auto bg-gray-700 hover:bg-gray-600 active:bg-gray-800 accent-orange-400 outline-orange-400 focus:outline-2 cursor-pointer rounded-sm'
                  >Add
                 </button>
 
