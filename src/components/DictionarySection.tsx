c779f8891e5c4e51cd3e564962aaf86fda8e2715
Nekit <nick.misiura@yandex.ru>
2025-03-19 18:57:52 +0300

    initial work on Dictionary section (mostly styling)

--- src/components/DictionarySection.tsx
+++ src/components/DictionarySection.tsx
@@ -1,8 +1,22 @@
+import { useState } from "react";
+
+// temp type for dictionary
+interface Dictionary {
+    [word: string]: string,
+}
+
 export default function DictionarySection() {
+    const [currentDictionaty, setCurrentDictionary] = useState<Dictionary>({}); // temp state for dictionary
 
     return (
         <div id="dictionary-section" className="text-center">
             <h2>Dictionaries</h2>
+            <select name="current-dictionary" id="current-dictionary" className="mt-4 min-w-20 cursor-pointer">
+                <option value="">Choose a dictionary</option>
+            </select>
+            <div className="mt-4 bg-[#505050] min-w-[300px] min-h-[250px] rounded-md">
+
+            </div>
         </div>
     );
 }
