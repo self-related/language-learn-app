03f69e2c05f3403c5d8406995184a1a1c1afb968
Nekit <nick.misiura@yandex.ru>
2025-04-06 10:38:57 +0300

    add flex-grow for lang selectors, prevent flex overflow with w-0, prevent text wrap for button with min-w-0

--- src/components/TranslateSection/LanguageSwitcher.tsx
+++ src/components/TranslateSection/LanguageSwitcher.tsx
@@ -18,16 +18,16 @@ const { languages, sourceLang, setSourceLang, targetLang, setTargetLang, switchL
     <div id="language-switcher" className="flex w-full justify-between">
 
     <select name="current-lang" id="current-lang" value={sourceLang}  onChange={(e) => setSourceLang(e.target.value)}
-            className="cursor-pointer p-2 w-[40%] bg-[#505050] hover:bg-[#606060] rounded-sm">
+            className="cursor-pointer p-2 grow w-0 bg-[#505050] hover:bg-[#606060] rounded-sm">
         {
             Object.keys(languages).map((key) => (<option key={key} value={key}> {languages[key]} </option>))
         }
     </select>
     
-    <button onClick={switchLangs} className="mx-2 w-[45px] cursor-pointer hover:bg-[#606060] p-1 rounded-sm">&lt;-&gt;</button>
+    <button onClick={switchLangs} className="mx-2 min-w-max cursor-pointer hover:bg-[#606060] p-1 rounded-sm">&lt;-&gt;</button>
     
     <select name="target-lang" id="target-lang" value={targetLang} onChange={(e) => setTargetLang(e.target.value)}
-            className="cursor-pointer p-2 w-[40%] bg-[#505050] hover:bg-[#606060] rounded-sm">
+            className="cursor-pointer p-2 grow w-0 bg-[#505050] hover:bg-[#606060] rounded-sm">
         {
             Object.keys(languages).slice(1).map((key) => (<option key={key} value={key}>{languages[key]}</option>))
         }
