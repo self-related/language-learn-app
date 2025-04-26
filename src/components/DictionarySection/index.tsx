b9bb0fb4ae4feecb7a1e4f7877fa059f0a9af5ae
Nekit <nick.misiura@yandex.ru>
2025-04-26 18:26:06 +0300

    styling of dictionary entries

--- src/components/DictionarySection/index.tsx
+++ src/components/DictionarySection/index.tsx
@@ -19,6 +19,8 @@ export default function DictionarySection() {
     return (
         <section id="dictionary-section" className="w-[60%] md:w-[30%]" >
             <h2 className='text-2xl mb-4'>Dictionaries</h2>
+
+            {/* dictionary selection panel */}
             <select name="current-dictionary" id="current-dictionary" onChange={handleDictionaryChange}
                     className="cursor-pointer bg-[#505050] hover:bg-[#606060] p-2 rounded-sm">
                 <option value="">Choose a dictionary</option>
@@ -26,12 +28,19 @@ export default function DictionarySection() {
                     Object.keys(dictionaries).map(key => (<option key={key} value={key}>{key}</option>))
                 }
             </select>
-            <div className="mt-4 p-2 bg-[#505050] min-w-[300px] min-h-[250px] rounded-md">
+
+            {/* dictionary div */}
+            <div className="mt-4 py-4 px-3 bg-[#505050] min-w-[300px] min-h-[250px] rounded-md flex flex-col gap-y-4">
                 {
                     currentDictionary && (
-                        <div>{dictionaries[currentDictionary].map((translation, index) => (
-                            <p key={index}><span className="text-red-300">{translation?.original}</span> - {translation?.mainTranslation} <button onClick={ () => handleRemoveButton(translation?.original) } className="text-white ml-1.5 rounded-sm w-4 h-4 text-[0.7rem] bg-gray-700 cursor-pointer hover:bg-gray-500 active:bg-gray-900">X</button></p>
-                        ))}</div>
+                        <>{dictionaries[currentDictionary].map((translation, index) => (
+                            <div key={index} className="flex justify-between items-center bg-[#414343] px-3 py-2 rounded-sm">
+                                <p className="max-w-[92%] break-words">
+                                   <span className="text-red-300">{translation?.original}</span> <br/> {translation?.mainTranslation} 
+                                </p>
+                                <button onClick={ () => handleRemoveButton(translation?.original) } className=" text-red-500 ml-1.5 rounded-sm h-6 aspect-square text-l  cursor-pointer hover:bg-orange-300 active:bg-orange-900">X</button>
+                            </div>
+                        ))}</>
                     )
                 }
             </div>
