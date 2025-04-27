4383e7b1e5967c5a50b00255696206de95293877
Nekit <nick.misiura@yandex.ru>
2025-04-27 18:24:56 +0300

    dictionary items - added current and target languages (also used &nbsp;), aligned delete button

--- src/components/DictionarySection/DictionaryEntry.tsx
+++ src/components/DictionarySection/DictionaryEntry.tsx
@@ -14,13 +14,21 @@ export default function DictionaryEntry({ translation, dictionary }: DictionaryE
         dispatch({type: "dictionarySlice/deleteTranslation", payload: {dictionary, original}});
     }
 
+    const [sourceLang, targetLang] = translation.dictionaryName!.split(" - "); // get languages of the current dictionary
+
     return (
-    <li className="flex justify-between items-center bg-[#414343] px-3 py-2 rounded-sm">
+    <li className="flex flex-col relative items-start bg-[#414343] px-3 py-2 rounded-sm">
+        <button onClick={ () => handleRemoveButton(translation?.original) } 
+            className="self-end absolute text-red-500 ml-1.5 rounded-sm h-6 aspect-square text-l  cursor-pointer hover:bg-orange-300 active:bg-orange-900"
+        >X</button>
+
         <p className="max-w-[92%] break-words">
-           <span className="text-red-300 block">{translation?.original}</span>
-           {translation?.mainTranslation} 
+           <span className="text-red-300">{sourceLang}:&nbsp;</span>{translation?.original}
         </p>
-        <button onClick={ () => handleRemoveButton(translation?.original) } className=" text-red-500 ml-1.5 rounded-sm h-6 aspect-square text-l  cursor-pointer hover:bg-orange-300 active:bg-orange-900">X</button>
+
+        <p className="max-w-[92%] break-words mt-1">
+            <span className="text-red-300">{targetLang}:&nbsp;</span>{translation?.mainTranslation}
+        </p>
     </li>
     );
 }
