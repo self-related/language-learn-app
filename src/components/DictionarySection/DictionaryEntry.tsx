a3ccda8e00eaf9b17c25dfe792197d5a4d60cbb0
Nekit <nick.misiura@yandex.ru>
2025-06-19 14:37:12 +0300

    Move entry dropdown into a separate component

--- src/components/DictionarySection/DictionaryEntry.tsx
+++ src/components/DictionarySection/DictionaryEntry.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { markLearned } from "../../redux/features/dictionary/dictionarySlice";
 import { useAppDispatch, useAppSelector } from "../../redux/store";
 import { TranslationResult } from "../../types";
+import ContextMenu from "./ContextMenu";
 
 interface DictionaryEntryProps {
     translation: TranslationResult,
@@ -101,16 +102,7 @@ export default function DictionaryEntry({ translation, dictionary }: DictionaryE
 
                 {/* dropdown menu */}
                 {
-                    showDropdown && (
-                        <div className="absolute right-[2rem] bottom-0 flex items-center gap-2 w-max rounded-sm bg-[#656565] text-white text-sm ">
-                            <p className="px-1.5 py-1 rounded-sm hover:bg-[#808080] active:bg-[#505050]">
-                                change
-                            </p>
-                            <p className="px-1.5 py-1 rounded-sm hover:bg-[#808080] active:bg-[#505050]">
-                                reverse
-                            </p>
-                        </div>
-                    )
+                    showDropdown && <ContextMenu />
                 }
 
             </button>
