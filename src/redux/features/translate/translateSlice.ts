487fa6b0b60c9b81eaed0d0104aac5c5959d9a70
Nekit <nick.misiura@yandex.ru>
2025-07-01 07:27:21 +0300

    Rework translateSlice

--- src/redux/features/translate/translateSlice.ts
+++ src/redux/features/translate/translateSlice.ts
@@ -1,15 +1,42 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { Languages, OtherTranslations } from "../../../types";
+import { languagesG } from "../../../consts";
 
 interface TranslateSliceState {
     inputText: string,
     outputText: string,
-    // TODO: add translateAutomatically, moreTranslations[], sourceLanguage and targetLanguage (en, es, etc), currentApi: {name, languageList} 
-    // TODO:  use loadFromLocalStorage() to load saved values
+
+    sourceLang: string,
+    targetLang: string,
+
+    original: string,
+    mainTranslation: string,
+    dictionaryName: string,
+
+    otherTranslations?: OtherTranslations[],
+    detectedLanguage?: string,
+    
+    translateAutomatically: boolean,
+
+    currentApi?: {id: string, languageMap: Languages},
 }
 
+// TODO:  use loadFromLocalStorage() to load saved values
 const initialState: TranslateSliceState = {
     inputText: "",
-    outputText: "",    
+    outputText: "",
+
+    sourceLang: "en",
+    targetLang: "es",
+
+    original: "",
+    mainTranslation: "",
+
+    dictionaryName: "",
+
+    translateAutomatically: true,
+    
+    currentApi: { id: "google", languageMap: languagesG }
 };
 
 export const translateSlice = createSlice({
@@ -23,9 +50,31 @@ export const translateSlice = createSlice({
             state.outputText = action.payload;
         },
         swapInputOutputText: (state: TranslateSliceState) => {
-            [state.inputText, state.outputText] = [state.outputText, state.inputText];
+            [state.original, state.mainTranslation] = [state.mainTranslation, state.original];
+        },
+        setOriginal: (state: TranslateSliceState, action: PayloadAction<string>) => { 
+            state.original = action.payload;
+        },
+        setMainTranslation: (state: TranslateSliceState, action: PayloadAction<string>) => { 
+            state.mainTranslation = action.payload;
+        },
+        setSourceLang: (state: TranslateSliceState, action: PayloadAction<string>) => {
+            state.sourceLang = action.payload;
+            // saveSettings(state);
+        },
+        setTargetLang: (state: TranslateSliceState, action: PayloadAction<string>) => {
+            state.targetLang = action.payload;
+            // saveSettings(state);
         },
     }
 });
 
-export const {swapInputOutputText, updateInputText, updateOutputText} = translateSlice.actions;
+export const {
+    swapInputOutputText, 
+    updateInputText, 
+    updateOutputText, 
+    setOriginal, 
+    setMainTranslation, 
+    setSourceLang, 
+    setTargetLang
+} = translateSlice.actions;
