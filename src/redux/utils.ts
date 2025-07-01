8b8a37dc4c0289e14c885c55669fdc11349ab7a7
Nekit <nick.misiura@yandex.ru>
2025-07-01 07:47:39 +0300

    Add functions to handle local storage

--- src/redux/utils.ts
+++ src/redux/utils.ts
@@ -1,10 +1,9 @@
-// TODO
-
-/*
-
-export function loadStateFromLocalStorage<T>(key: string, initialState: T) {
+export function loadStateFromLocalStorage<T>(key: string): T | null {
     const localStorageValue = localStorage.getItem(key);
     const parsedValue = localStorageValue ? JSON.parse(localStorageValue) : null;
+    return parsedValue as T | null;
 }
 
-*/
+export function saveToLocalStorage<T>(key: string, currentState: T) {
+    localStorage.setItem(key, JSON.stringify(currentState));
+};
